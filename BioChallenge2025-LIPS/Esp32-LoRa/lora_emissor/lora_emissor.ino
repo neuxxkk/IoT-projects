1e75259176b1ea0246f857071391860a80caa18d
neuxxkk <vitornms@gmail.com>
2025-05-14 23:09:25 -0300

    Finished, now, make other sensors

--- BioChallenge2025-LIPS/Esp32-LoRa/lora_emissor/lora_emissor.ino
+++ BioChallenge2025-LIPS/Esp32-LoRa/lora_emissor/lora_emissor.ino
@@ -1,26 +1,14 @@
 #include <WiFi.h>
-#include <Wire.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
 #include <LoRa.h>
 #include <SPI.h>
+#include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SENSOR 13  // Pino do sensor de umidade
-
-/* Definições do OLED */
-#define OLED_SDA_PIN    4
-#define OLED_SCL_PIN    15
-#define SCREEN_WIDTH    128 
-#define SCREEN_HEIGHT   64  
-#define OLED_ADDR       0x3C 
-#define OLED_RESET      16
-
-/* Offset de linhas no display OLED */
-#define OLED_LINE1      0
-#define OLED_LINE2      SCREEN_HEIGHT/2 + 20
-#define OLED_LINE3      OLED_LINE2 + 20
-
-/* Definições para comunicação com rádio LoRa */
+// Pinos do LoRa
 #define SCK_LORA        5
 #define MISO_LORA       19
 #define MOSI_LORA       27
@@ -31,69 +19,102 @@
 #define HIGH_GAIN_LORA  14    // 0 ~ 20dBm
 #define BAND            915E6 // 915MHz
 
-// Valores da calibragem
-#define valor_seco 2700
-#define valor_umido 1400
+// Pino do sensor de umidade do solo
+#define SENSOR_PIN      36
 
-/* objeto do display */
+// Definições do OLED
+#define OLED_SDA_PIN    4
+#define OLED_SCL_PIN    15
+#define SCREEN_WIDTH    128
+#define SCREEN_HEIGHT   64
+#define OLED_ADDR       0x3C
+#define OLED_RESET      16
+
+// linhas do OLED
+#define OLED_LINE1      0
+#define OLED_LINE2      40
+#define OLED_LINE3      50
+
+// Identificadores
+#define DEBUG_SERIAL_BAUDRATE 115200
+#define TIMEOUT_MS 2000 // Tempo de espera Handshake
+#define TIMEOUT_GLOBAL_MS 5000 // Espaçamento entre buscas por central   
+
+// PRECISA CALIBRAR!
+#define VALOR_SECO  2400
+#define VALOR_UMIDO 1700
+
+//Struct para dados do LoRa
+struct LoraData {
+  float umidade;
+} data;
+
+// Variáveis globais
+bool central_exists;
+bool central_exists_aux;
+unsigned long last_central_check;
+
+// WiFi
+const char* ssid     = "vitornms";
+const char* password = "qwerasdf";
+// Parâmetros de rede para ter IP estático
+IPAddress local_IP(192,168,172,65);    // 172 é o IP do ponto de acesso
+IPAddress gateway(192,168,172,1);
+IPAddress subnet(255,255,255,0);   
+
+// Server declaration
+AsyncWebServer server(80);
+
+// Outros
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// Protótipos
-void display_init();
-void escreve_display(float umid);
-void escreve_serial(float umid);
-void envia_lora(float umid);
-bool init_comunicacao_lora();
 
-void display_init() {
+// all-time functions
+bool display_init();
+bool lora_chip_init();
+float sensor_read();
+void central_check();
+
+// as-central functions
+void server_init();
+bool wifi_connect();
+void display_write_central();
+
+// as-component functions
+void display_write_component();
+void lora_send();
+
+
+/*
+  ALL-TIME FUNCTIONS
+*/
+
+/*
+    Inicia o display OLED
+    - Inicia I2C nos pinos definidos
+    - Inicia o OLED
+*/
+bool display_init() {
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
     Serial.println("[LoRa Receiver] Falha ao inicializar OLED");
-  } else {
-    Serial.println("[LoRa Receiver] OLED inicializado");
-    display.clearDisplay();
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
+    return false;
   }
+  Serial.println("[LoRa Receiver] OLED inicializado");
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  return true;
 }
 
-void escreve_serial(float umid) {
-  char mensagem[100];
-
-  // limpa tela Serial
-  for (int i = 0; i < 80; i++) {
-    Serial.println();
-  }
-
-  // imprime umidade atual
-  snprintf(mensagem, sizeof(mensagem), "- Umidade atual: %.2f%%", umid);
-  Serial.println(mensagem);
-}
-
-void escreve_display(float umid) {
-  char buf[16];
-  snprintf(buf, sizeof(buf), "%.2f%%", umid);
-
-  // apaga apenas a segunda linha (10px de altura)
-  display.fillRect(0, OLED_LINE2, SCREEN_WIDTH, 10, BLACK);
-
-  display.setTextSize(1);
-  display.setCursor(0, OLED_LINE2);
-  display.print("Umidade: ");
-  display.print(buf);
-  display.display();
-}
-
-void envia_lora(float umid) {
-  LoRa.beginPacket();
-  LoRa.write((uint8_t*)&umid, sizeof(umid));
-  LoRa.endPacket();
-}
-
-bool init_comunicacao_lora() {
+/*
+    Inicia comunicação com chip LoRa
+    - Inicia SPI
+    - Inicia LoRa
+*/
+bool lora_chip_init() {
   Serial.println("[LoRa Sender] Iniciando LoRa...");
   SPI.begin(SCK_LORA, MISO_LORA, MOSI_LORA, SS_PIN_LORA);
   LoRa.setPins(SS_PIN_LORA, RESET_PIN_LORA, LORA_DEFAULT_DIO0_PIN);
-
   if (!LoRa.begin(BAND)) {
     Serial.println("[LoRa Sender] Falha no LoRa, retry em 1s");
     return false;
@@ -103,49 +124,200 @@ bool init_comunicacao_lora() {
   return true;
 }
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(SENSOR, INPUT);
+/*
+    Lê o valor do sensor de umidade do solo
+    - Converte o valor para porcentagem
+*/
+float sensor_read() {
+  float raw = analogRead(SENSOR_PIN);
+  float perc = map(raw, VALOR_SECO, VALOR_UMIDO, 0, 100);
+  float umidade_perc = constrain(perc, 0, 100);
+  return umidade_perc;
+}
 
-  // 1) Inicializa I2C nos pinos definidos
-  Wire.begin(OLED_SDA_PIN, OLED_SCL_PIN);
 
-  // 2) Inicializa o OLED
-  display_init();
+/*
+    Verifica se existe central
+    - Realiza Handshake com a central
+*/
+void central_check() {
+  last_central_check = millis();
+  central_exists = false;
+  LoRa.beginPacket(); LoRa.print("PING_CENTRAL"); LoRa.endPacket();
+  Serial.printf("\n\n[LoRa] Checking for central...\n");
+  unsigned long start = millis();
+  while (millis() - start < TIMEOUT_MS) {
+    if (LoRa.parsePacket()) {
+      String resp = LoRa.readString();
+      if (resp == "ACK_CENTRAL") {
+        central_exists = true;
+        break;
+      }
+    }
+  }
+  if (central_exists){
+    if (!central_exists_aux) turn_into_component();
+    central_exists_aux = true;
+    Serial.printf("[LoRa] Central found!\n\n");
+  }
+  else{
+    if (central_exists_aux)  turn_into_central();
+    central_exists_aux = false;
+    Serial.printf("[LoRa] No central found.\n\n");
+  }
+}
 
-  // Mensagem inicial
+
+/*
+  AS-CENTRAL FUNCTIONS
+*/
+/*
+    Torna o dispositivo em central
+    - Conecta ao WiFi
+    - Inicia o servidor
+*/
+void turn_into_central(){
+  wifi_connect();
+  server_init();
+}
+
+
+/*
+    Inicia o servidor
+    - Monta o sistema de arquivos LittleFS
+    - Define as rotas do servidor
+*/
+void server_init() {
+  if (!LittleFS.begin()) {
+    Serial.println("Erro ao montar LittleFS");
+    return;
+  }
+  
+  server.begin();
+  // disponibiliza o url "/" com o conteúdo da String abaixo
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  //  salva umidade no caminho "/umidade"
+  server.on("/umidade", HTTP_GET, [](AsyncWebServerRequest *request) {
+    char buf[16];
+    dtostrf(data.umidade, 6, 2, buf);
+    request->send(200, "text/plain", buf);
+  });
+
+  Serial.println("[LoRa Sender] Servidor HTTP iniciado");
+}
+
+
+/*
+    Conecta ao WiFi
+    - Configura IP estático
+*/
+bool wifi_connect(){
+  WiFi.mode(WIFI_STA);
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+    Serial.println("Falha ao configurar IP estático");
+  }
+  WiFi.begin(ssid, password);
+  Serial.println("Tentando conectar ao WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print('.');
+  }
+  Serial.print("\nConectado com IP: ");
+  Serial.println(WiFi.localIP());
+  return true;
+}
+
+
+/*
+    Atualiza display sendo central
+*/
+void display_write_central() {
   display.clearDisplay();
-  display.setCursor(SCREEN_WIDTH, OLED_LINE1);
-  display.print("EMISSOR");
+  display.setTextSize(2);
+  display.setCursor(0, OLED_LINE1);
+  display.printf("EMISSOR");
   display.setTextSize(1);
   display.setCursor(0, OLED_LINE2);
-  display.print("Ativo. Aguarde...");
+  display.printf("Umidade: %.2f%%", data.umidade);
+  display.setCursor(0, OLED_LINE3);
+  display.print("IP: ");
+  display.print(WiFi.localIP());
   display.display();
+}
 
-  // 3) Inicializa LoRa (bloqueante até sucesso)
-  while (!init_comunicacao_lora()) {
-    delay(1000);
-  }
 
-  display.fillRect(0, OLED_LINE2, SCREEN_WIDTH, 30, BLACK);
+/*
+  AS-COMPONENT FUNCTIONS
+*/
+/*
+    Torna o dispositivo em componente
+    - Desconecta do WiFi
+    - Desliga o servidor
+*/
+void turn_into_component(){
+  server.end();
+  WiFi.disconnect();
+  WiFi.mode(WIFI_OFF);
+}
+
+
+/*
+    Atualiza display sendo componente
+*/
+void display_write_component() {
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setCursor(0, OLED_LINE1);
+  display.printf("COMPONENTE");
+  display.setTextSize(1);
+  display.setCursor(0, OLED_LINE2);
+  display.printf("Umidade: %.2f%%", data.umidade);
+  display.display();
+}
+
+
+/*
+    Envia os dados via LoRa
+    - Envia o pacote com os dados do sensor
+*/
+void lora_send() {
+  LoRa.beginPacket();
+  LoRa.write((uint8_t*)&data, sizeof(data));
+  LoRa.endPacket();
+}
+
+/*
+
+
+*/
+void setup() {
+  Serial.begin(DEBUG_SERIAL_BAUDRATE);
+  pinMode(SENSOR_PIN, INPUT);
+  Wire.begin(OLED_SDA_PIN, OLED_SCL_PIN);
+
+  while (!display_init()) delay(500);
+
+  while (!lora_chip_init()) delay(1000);
+
+  central_check();
+  if (central_exists) turn_into_component();
+  else if (!central_exists) turn_into_central();
 }
 
 void loop() {
-  // 4) Leitura e escala de 0–100%
-  int raw = analogRead(SENSOR);
-  
-  // Mapeia a leitura para uma escala de 0% (seco) a 100% (úmido)
-  int umidadePercentual = map(raw, valor_seco, valor_umido, 0, 100);
-  // Garante que o valor esteja entre 0% e 100%
-  float umid = constrain(umidadePercentual, 0, 100);
 
-  // 5) Se algo der errado na leitura
-  if (isnan(umid)) {
-    Serial.println("Erro ao ler sensor de umidade!");
-  } else {
-    escreve_serial(umid);
-    escreve_display(umid);
-    envia_lora(umid);
+  if (millis() - last_central_check > TIMEOUT_GLOBAL_MS) central_check();
+
+  data.umidade = sensor_read();
+
+  if (central_exists){
+    display_write_component();
+    lora_send();
+  }else{
+    display_write_central();
   }
 
   delay(1500);
--- BioChallenge2025-LIPS/Esp32-LoRa/lora_receptor/lora_receptor.ino
+++ BioChallenge2025-LIPS/Esp32-LoRa/lora_receptor/lora_receptor.ino
@@ -1,32 +1,25 @@
-// Web librarys
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
-
-// Instanciação do objeto da classe AsyncWebServer
-AsyncWebServer server(80);
-
-// LoRa
 #include <LoRa.h>
 #include <SPI.h>
-
-// OLED LoRa
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-/* Definicoes para comunicação com radio LoRa */
-#define SCK_LORA           5
-#define MISO_LORA          19
-#define MOSI_LORA          27
-#define RESET_PIN_LORA     14
-#define SS_PIN_LORA        18
+// Pinos do LoRa
+#define SCK_LORA        5
+#define MISO_LORA       19
+#define MOSI_LORA       27
+#define RESET_PIN_LORA  14
+#define SS_PIN_LORA     18
 
-#define HIGH_GAIN_LORA     20  /* dBm */
-#define BAND               915E6  /* 915MHz de frequencia */
+// Parâmetros LoRa
+#define HIGH_GAIN_LORA  14    // 0 ~ 20dBm
+#define BAND            915E6 // 915MHz
 
-/* Definicoes do OLED */
+// Definicoes do OLED 
 #define OLED_SDA_PIN    4
 #define OLED_SCL_PIN    15
 #define SCREEN_WIDTH    128 
@@ -34,83 +27,199 @@ AsyncWebServer server(80);
 #define OLED_ADDR       0x3C 
 #define OLED_RESET      16
 
-/* Offset de linhas no display OLED */
+// linhas do OLED
 #define OLED_LINE1      0
-#define OLED_LINE2      OLED_LINE1 + 40
-#define OLED_LINE3      OLED_LINE2 + 10
+#define OLED_LINE2      40
+#define OLED_LINE3      50
 
-/* Definicoes gerais */
+// Struct para dados do LoRa
+struct LoraData {
+  float umidade;
+}data;
+
+// Identificadores
 #define DEBUG_SERIAL_BAUDRATE    115200
 
-/* Variaveis e objetos globais */
+// WIFI
+const char* ssid     = "vitornms";
+const char* password = "qwerasdf";
+// Parâmetros de rede para ter IP estático
+IPAddress local_IP(192,168,172,65);    // 172 é o IP do ponto de acesso
+IPAddress gateway(192,168,172,1);
+IPAddress subnet(255,255,255,0);
+
+// Server declaration
+AsyncWebServer server(80);
+
+// Outros
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// Others
-int rssi;
-float umidade = 0.00;
 
-// WIFI
-const char* ssid     = "JV";
-const char* password = "galoDoido";
-
-// Funcoes
+// Funções de inicialização
+bool display_init(void);
+bool lora_chip_init(void);
 void server_init(void);
-void conect_wifi(void);
-void display_init(void);
-void escreve_display(float);
-bool init_comunicacao_lora(void);
-void get_lora_data(void);
 
-void envia_web(float);
+// Funções de primeiro contato
+bool wifi_connect(void);
+void default_display(void);
+
+// Funções de leitura
+void component_read(void);
+bool lora_interpret(void);
+
+// Funções de escrita e envio
+void display_write(void);
+
 
 /*
-    TRY begin Server
+    Funcoes de inicialização
 */
+/*
+    Inicia o display OLED
+    - Inicia I2C nos pinos definidos
+    - Inicia o OLED
+*/
+bool display_init() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println("[LoRa Receiver] Falha ao inicializar OLED");
+    return false;
+  } else {
+    Serial.println("[LoRa Receiver] OLED inicializado");
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    default_display();
+    return true;
+  }
+}
 
+/*
+    Inicia comunicação com chip LoRa
+    - Inicia SPI
+    - Inicia LoRa
+*/
+bool lora_chip_init() {
+  Serial.println("[LoRa Sender] Iniciando LoRa...");
+  SPI.begin(SCK_LORA, MISO_LORA, MOSI_LORA, SS_PIN_LORA);
+  LoRa.setPins(SS_PIN_LORA, RESET_PIN_LORA, LORA_DEFAULT_DIO0_PIN);
+
+  if (!LoRa.begin(BAND)) {
+    Serial.println("[LoRa Sender] Falha no LoRa, retry em 1s");
+    return false;
+  }
+  LoRa.setTxPower(HIGH_GAIN_LORA);
+  Serial.println("[LoRa Sender] LoRa OK");
+  return true;
+}
+
+/*
+    Begin Server
+    - Inicia o servidor web
+    - Cria ponto de acesso
+*/
 void server_init(){
-    // Servidor começa à ouvir os clientes
     server.begin();
-      // disponibiliza o url "/" com o conteúdo da String abaixo
+    // disponibiliza o url "/" com o conteúdo da String abaixo
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(LittleFS, "/index.html", "text/html");
     });
+
+    //  salva umidade no caminho "/umidade"
+    server.on("/umidade", HTTP_GET, [](AsyncWebServerRequest *request) {
+        char buf[16];
+        dtostrf(data.umidade, 6, 2, buf);
+        request->send(200, "text/plain", buf);
+    });
+
+    Serial.println("[LoRa Sender] Servidor HTTP iniciado");
 }
 
 /*
-    TRY connect to WIFI
+    Funções de primeiro contato
 */
-void conect_wifi(){
-    WiFi.begin(ssid, password);
-    Serial.println(WiFi.localIP());
+/*
+    Connect to WIFI
+    - Tenta conectar ao wifi
+*/
+bool wifi_connect(){
+  WiFi.mode(WIFI_STA);
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+    Serial.println("Falha ao configurar IP estático"); 
+  }
+  WiFi.begin(ssid, password);
+  Serial.println("Tentando conectar ao WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print('.');
+  }
+  Serial.printf("\nConectado ao wifi %s com IP: ", ssid);
+  Serial.println(WiFi.localIP());  
+  return true;
 }
 
-/* 
-    Funcao: inicializa comunicacao com o display OLED
-*/ 
-void display_init(void) {
-    if(!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) 
-    {
-        Serial.println("[LoRa Receiver] Falha ao inicializar comunicacao com OLED");        
+/*
+    Função que printa deafult no OLED
+*/
+void default_display() {
+    display.clearDisplay();
+    display.setCursor(SCREEN_WIDTH, OLED_LINE1);
+    display.print("RECEPTOR");
+    display.setTextSize(1);
+    display.setCursor(0, OLED_LINE2);
+    display.print("Esperando componente. Aguarde...");
+    display.display();
+}
+
+/*
+    Funcoes de leitura
+*/
+/*
+    Função que le radios LoRa
+    - Verifica se chegou alguma informação (do tamanho esperado, STRUCT - more data-)
+*/
+void component_read() {
+    LoRa.readBytes((uint8_t*)&data, sizeof(data));
+}
+
+
+/*
+    Função que interpreta os dados recebidos
+    - Verifica se chegou algum pacote
+    - Se o pacote for do tamanho esperado, lê os dados
+    - Se não, verifica se é um PING_CENTRAL e responde com ACK_CENTRAL
+*/
+bool lora_interpret(){
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+        if (packetSize == sizeof(LoraData)){
+            component_read();
+            Serial.printf("\n[LoRa] Recebido LoraData -> Posting on IP: \n");
+            Serial.println(WiFi.localIP());
+        }else {
+            String msg = LoRa.readString();
+            msg.trim();
+            
+            // 1) Se for handshake de descoberta, responde ACK
+            if (msg == "PING_CENTRAL") {
+                Serial.println("[LoRa] Recebido PING_CENTRAL -> Respondendo ACK_CENTRAL");
+                LoRa.beginPacket();
+                LoRa.print("ACK_CENTRAL");
+                LoRa.endPacket();
+            }
+        }
+        return true;
     }
-    else
-    {
-        Serial.println("[LoRa Receiver] Comunicacao com OLED inicializada com sucesso");
-    
-        /* Limpa display e configura tamanho de fonte */
-        display.clearDisplay();
-        display.setTextSize(2);
-        display.setTextColor(WHITE);
-    }
+    return false;
 }
 
 /*
     Funcao que printa no OLED
 */
-void escreve_display(float umid) {
+void display_write() {
     char buf[16];
-    snprintf(buf, sizeof(buf), "%.2f%%", umid);
+    snprintf(buf, sizeof(buf), "%.2f%%", data.umidade);
 
-    // apaga apenas a segunda linha (10px de altura)
     display.fillRect(0, OLED_LINE2, SCREEN_WIDTH, 20, BLACK);
 
     display.setTextSize(1);
@@ -123,89 +232,26 @@ void escreve_display(float umid) {
     display.display();
 }
 
-/*
-    Funcao: inicia comunicação com chip LoRa
-*/
-bool init_comunicacao_lora(void) {
-    bool status_init = false;
-    Serial.println("[LoRa Receiver] Tentando iniciar comunicacao com o radio LoRa...");
-    SPI.begin(SCK_LORA, MISO_LORA, MOSI_LORA, SS_PIN_LORA);
-    LoRa.setPins(SS_PIN_LORA, RESET_PIN_LORA, LORA_DEFAULT_DIO0_PIN);
-    
-    if (!LoRa.begin(BAND)) {
-        Serial.println("[LoRa Receiver] Comunicacao com o radio LoRa falhou. Nova tentativa em 1 segundo...");        
-        delay(1000);
-        status_init = false;
-    }else {
-        /* Configura o ganho do receptor LoRa para 20dBm, o maior ganho possível (visando maior alcance possível) */ 
-        LoRa.setTxPower(HIGH_GAIN_LORA); 
-        Serial.println("[LoRa Receiver] Comunicacao com o radio LoRa ok");
-        status_init = true;
-    }
-
-    return status_init;
-}
-
-/*
-    FUNCAO envia dados para web
-*/
-
-void envia_web(float umidade) {
-    //
-}
-
-/* 
-    Funcao de setup 
-*/
 void setup() {
     Serial.begin(DEBUG_SERIAL_BAUDRATE);
-    while (!Serial);
 
-        // Beggining LittleFS
+    Wire.begin(OLED_SDA_PIN, OLED_SCL_PIN);
+
+    display_init();
+
     if(!LittleFS.begin()){
         Serial.println("An Error has occurred while mounting LittleFS");
         return;
     }
 
-    // Wifi connection
-    conect_wifi();
+    wifi_connect();
     server_init();
 
-    // ** Nova rota que retorna o valor da umidade em texto puro **
-    server.on("/umidade", HTTP_GET, [](AsyncWebServerRequest *request) {
-        char buf[16];
-        dtostrf(umidade, 6, 2, buf);
-        request->send(200, "text/plain", buf);
-    });
-
-    // Configuracao display OLED
-    Wire.begin(OLED_SDA_PIN, OLED_SCL_PIN);
-    display_init();
-    // Mensagem inicial
-    display.clearDisplay();
-    display.setCursor(SCREEN_WIDTH, OLED_LINE1);
-    display.print("RECEPTOR");
-    display.setTextSize(1);
-    display.setCursor(0, OLED_LINE2);
-    display.print("Ativo. Aguarde...");
-    display.display();
-
-    /* Tenta, até obter sucesso, comunicacao com o chip LoRa */
-    while(init_comunicacao_lora() == false);  
-    display.fillRect(0, OLED_LINE2, SCREEN_WIDTH, 30, BLACK); // fim do aguarde
+    while(lora_chip_init() == false);
 }
 
-/* Programa principal */
 void loop() {
-    /* Verifica se chegou alguma informação (do tamanho esperado, STRUCT - more data-) */
-    if (LoRa.parsePacket()) {
-        float umidadeRecebida;
-        LoRa.readBytes((uint8_t*)&umidadeRecebida, sizeof(umidadeRecebida));
-        umidade = umidadeRecebida;
-
-        escreve_display(umidade);
-        Serial.print("\nUmidade do solo: ");
-        Serial.print(umidade);
-        Serial.print("%");
-    }
+    while (!lora_interpret());
+    display_write();
+    delay(10);
 }
